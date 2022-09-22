0c8fd7f16b82b5da5c9d8e55a9a602e4b75ccbf0
Timo Kinnunen <timo.kinnunen@bredband.net>
2022-09-22 15:24:34 +0200

    Add project files.

---
+++ EratosthenesSieveNow/Models/PrimeNumber.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace EratosthenesSieveNow.Models
+{
+    [Table("PrimeNumbers")]
+    public class PrimeNumberItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Unique]
+        public string PrimeNumber { get; set; }
+    }
+}
